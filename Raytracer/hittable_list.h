0f8a6267f7ceafa2751e91ace81dd6ba7975cace
Dudestin <splashkakaponear@gmail.com>
2020-01-12 23:54:48 +0900

    fix parameter of ray()

--- Raytracer/hittable_list.h
+++ Raytracer/hittable_list.h
@@ -5,7 +5,7 @@
 class hittable_list : public hittable {
 public:
     hittable_list() {};
-    hittable_list(std::vector<std::unique_ptr<hittable>>&& l) { list = std::move(l); }
+    hittable_list(std::vector<std::unique_ptr<hittable>>&& l) :list(std::move(l)) {}
     ~hittable_list() { std::vector<std::unique_ptr<hittable>>().swap(list); };
     virtual bool hit(
         const ray& r, double tmin, double tmax, hit_record& rec) const;
